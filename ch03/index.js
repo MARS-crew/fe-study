a44a38506889aa52d920137d59faf6a4c401fd70
pinomaker-hoo <inhoo23@naver.com>
2024-09-07 09:55:01 +0900

    feat : ch03

---
+++ ch03/index.js
@@ -0,0 +1,12 @@
+/**
+ * a, b 두 개의 숫자를 받아서 더한 값을 반환하는 함수
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number}
+ */
+function add(a, b) {
+  return a + b;
+}
+
+const response = add(1, 2);
+console.log(response + 3);
